e3f2673295c04408b43f2f70a4b2565b763c86e2
ColdFire <fanilgandhi@gmail.com>
2017-11-02 01:56:16 +0530

    Practical 12 Completed

---
+++ practical-12.py
@@ -0,0 +1,31 @@
+'''
+Implement LCS problem
+'''
+
+
+def LCS(x, y):
+    subsequence = ''
+    if len(x) > len(y):
+        x, y = y, x
+
+    solution = [0] + [0] * len(x)
+    prev = solution
+    if x[0] == y[0]:
+        subsequence += y[0]
+
+    for j in range(len(y)):
+        for i in range(1, len(x) + 1):
+            if (x[i - 1] == y[j]):
+                solution[i] = max(0, prev[i - 1] + 1)
+            else:
+                solution[i] = max(solution[i - 1], solution[i])
+
+        if solution != prev:
+            subsequence += y[j]
+        prev = solution[:]
+
+    print("The Longest Common Subsequence between {0} and {1} is {2} of {3} characters".format(x, y, subsequence, solution[-1]))
+
+
+if __name__ == '__main__':
+    LCS("abcdef", "pqrabcdef")
